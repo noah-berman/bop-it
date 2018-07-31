27ce66be8f4037602bd6a96224d236f6ae20f139
noah-berman <noahberm@gmail.com>
2018-07-31 15:43:50 -0400

    added in starter canvas

---
+++ src/index.js
@@ -0,0 +1,19 @@
+document.addEventListener('DOMContentLoaded', function(event) {
+  const body = document.getElementsByTagName('BODY')[0];
+  const canvas = document.getElementById('myCanvas')
+
+  console.log(body);
+
+
+  body.appendChild(test);
+
+
+
+
+
+
+
+
+
+
+})
