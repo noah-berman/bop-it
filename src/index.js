21de608b68b66e954170d5b03dd5153b05d9f727
noah-berman <noahberm@gmail.com>
2018-08-01 14:21:04 -0400

    added HTML and CSS and selectable DOM elements

--- src/index.js
+++ src/index.js
@@ -1,16 +1,37 @@
 document.addEventListener('DOMContentLoaded', function(event) {
+  const bopit = document.getElementsByClassName('bopit')[0]
+  const bopitBox = document.getElementById('box')
+  const scrollbar = document.getElementById('scrollbar')
 
-  const body = document.getElementsByTagName('BODY')[0];
-  const canvas = document.getElementById('myCanvas')
-  const ctx = canvas.getContext("2d")
+  function rotateBopit() {
+    if (document.getElementsByClassName('bopit')[0].id === 'bopit-angle-centered') {
+      console.log(document.getElementsByClassName('bopit')[0]);
+      document.getElementsByClassName('bopit')[0].setAttribute("id", 'bopit-angle-right')
+    }
+    else if (document.getElementsByClassName('bopit')[0].id === 'bopit-angle-right') {
+      console.log(document.getElementsByClassName('bopit')[0]);
+      document.getElementsByClassName('bopit')[0].setAttribute("id", 'bopit-angle-left')
+    }
+    else if (document.getElementsByClassName('bopit')[0].id === 'bopit-angle-left') {
+      console.log(document.getElementsByClassName('bopit')[0]);
+      document.getElementsByClassName('bopit')[0].setAttribute("id", 'bopit-image-up')
+    }
+    else if (document.getElementsByClassName('bopit')[0].id === 'bopit-image-up') {
+      console.log(document.getElementsByClassName('bopit')[0]);
+      document.getElementsByClassName('bopit')[0].setAttribute("id", 'bopit-image-down')
+    }
+    else if (document.getElementsByClassName('bopit')[0].id === 'bopit-image-down') {
+      console.log(document.getElementsByClassName('bopit')[0]);
+      document.getElementsByClassName('bopit')[0].setAttribute("id", 'bopit-angle-centered')
+    }
+  }
 
+  // setInterval(rotateBopit, 500)
 
-  ctx.beginPath();
-  ctx.rect(20, 40, 50, 50);
-  ctx.fillStyle = "#FF0000";
-  ctx.fill();
-  ctx.closePath();
+  function boxlog() {console.log('Bop it!')}
+  function twistlog() {console.log('Twist it!')}
 
+  bopitBox.addEventListener('click', boxlog);
 
-
+  scrollbar.addEventListener('scroll', twistlog);
 })
